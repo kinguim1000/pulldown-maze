c572405c084f78ad6ec5924094abbbad5f44eef7
Your Name <you@example.com>
2022-05-06 18:51:59 -0300

    lx16a

--- aula/newtest/test.py
+++ aula/newtest/test.py
@@ -1,4 +1,3 @@
-import keyboard
 from pylx16a.lx16a import *
 import time
 
@@ -6,23 +5,38 @@ LX16A.initialize("/dev/ttyUSB0")
 
 try:
     servo1 = LX16A(1)
-    servo2 = LX16A(3)
-    servo1.disable_torque
-    servo2.disable_torque
+    #servo2 = LX16A(3)
+    #servo1.disable_torque
+    #servo2.disable_torque
 
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
     quit()
 
 t = 0
+state = True
 while True:
-    if keyboard.read_key()=="a":
-        servo1.motor_mode(100)
+   
+    #servo1.motor_mode(-1000)
+
+    
     # servo1.servo_mode()
     # servo.move(180)
     
-        print(servo1.get_physical_angle())
+    print(servo1.get_physical_angle())
     # print(servo1.get_commanded_angle())
         # print(True,servo2.get_motor_speed)
-    
-    
+    #if servo2.get_physical_angle() < 2000:
+     #   if state == True:
+      #      state = False
+    servo1.motor_mode(0)
+        #    time.sleep(0.4)
+        #else:
+
+            #state = True
+            #servo2.motor_mode(-200)
+            #time.sleep(0.4)
+#    if state:
+ #       servo2.motor_mode(-200)
+  #  else:
+   #     servo2.motor_mode(200)
