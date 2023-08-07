ba8814c6458f38b8ed081a9c265cb5b477e32f63
kinguim's pc <kinguim1000@gmail.com>
2023-08-07 18:07:02 -0300

    madwick

--- code/odom/test3107.py
+++ code/odom/test3107.py
@@ -275,7 +275,7 @@ class Robo: #Classe que vai segurar... Tudo... em teoria vai ajudar na organiza�
             if(self._acumulador[1] >= 1):
                 string = string +str(self._acumulador[1])+" rotações completas e "
             string = string +str(self._intencao[1])+" cm"
-        print(string + str(self._imu.pitch()))
+        print(string + str(self._imu.yaw()))
         return
 
     def Virar(self,lado,angulo):
