7cc8955f702428395150ac55c61515d72c306cb1
kinguim's pc <kinguim1000@gmail.com>
2023-08-10 18:06:31 -0300

    auto

--- code/odom/test0808.py
+++ code/odom/test0808.py
@@ -30,44 +30,53 @@ motor1 = motor(1)
 motor2 = motor(4)
 motor3 = motor(2)
 motor4 = motor(3)
-matrix = [motor1.pos(),motor2.pos(),motor3.pos(),motor4.pos()]#pra n dar problema de escopo
-def atualizar():
-    matrix = [motor1.pos(),motor2.pos(),motor3.pos(),motor4.pos()]
+# posicao = [motor1.pos(),motor2.pos(),motor3.pos(),motor4.pos()]#pra n dar problema de escopo
+# def atualizar():
+#     posicao = [motor1.pos(),motor2.pos(),motor3.pos(),motor4.pos()]
+def posicao(num):
+    if num == 1:
+        return motor1.pos()
+    if num == 2: 
+        return motor2.pos()
+    if num == 3:
+        return motor3.pos()
+    if num == 4:
+        return motor4.pos()
 def encoder():
     atualizar()
     
-    if(matrix[3] > 45 and matrix[3] < 273): #usar outro motor pra pegar pos
-        print([matrix[3],3])
-        return [matrix[3],3]
-    elif(matrix[2]> 45 and matrix[2] < 273):
-        print([matrix[2],2])
-        return [matrix[2],2]
-    elif(matrix[1]> 45 and matrix[1] < 273):
-        print([matrix[1],1])
-        return [matrix[1],1]
+    if(posicao(3) > 45 and posicao(3) < 273): #usar outro motor pra pegar pos
+        print([posicao(3),3])
+        return [posicao(3),3]
+    elif(posicao(2)> 45 and posicao(2) < 273):
+        print([posicao(2),2])
+        return [posicao(2),2]
+    elif(posicao(1)> 45 and posicao(1) < 273):
+        print([posicao(1),1])
+        return [posicao(1),1]
     else:
-        print([matrix[0],0])
-        return [matrix[0],0]
+        print([posicao(0),0])
+        return [posicao(0),0]
 
 def turnRight(vel):
     idMotor = encoder()[1];#isso aqui tem que atualizar a parte de cima pra ser os motores que vão pra frente 
     start = encoder()[0];
 
     atualizar()
-    if(matrix[idMotor] > 90):#garantir( sendo que acima ja tem 4 verificações de qual usar ( tem que ver se para tras tbm vale os valores))
-        while(matrix[idMotor] > start-90): #não sei se é maior ou menor que
+    if(posicao(idMotor) > 90):#garantir( sendo que acima ja tem 4 verificações de qual usar ( tem que ver se para tras tbm vale os valores))
+        while(posicao(idMotor) > start-90): #não sei se é maior ou menor que
             atualizar()
             irDireita(vel)
     else:
-        resto = matrix[idMotor] % 360
+        resto = posicao(idMotor) % 360
         
-        while(matrix[idMotor] > 1):
+        while(posicao(idMotor) > 1):
             atualizar()
             irDireita(vel)
         idMotor = encoder()[1];#isso aqui tem que atualizar a parte de cima pra ser os motores que vão pra frente 
         start = encoder()[0];
         
-        while(matrix[idMotor] > start - resto ):
+        while(posicao(idMotor) > start - resto ):
             atualizar()
             irDireita(vel)
 
@@ -76,22 +85,22 @@ def turnLeft(vel):
     start = encoder()[0];#teoria escolhe ja o melhor motor para fazer essa decisão
 
     atualizar()
-    if(matrix[idMotor] < 228):
-        while(matrix[idMotor] < start+90): #não sei se é maior ou menor que
+    if(posicao(idMotor) < 228):
+        while(posicao(idMotor) < start+90): #não sei se é maior ou menor que
             atualizar()
-            print(matrix[idMotor])
+            print(posicao(idMotor))
             irEsquerda(vel)
     else:
         
-        resto = matrix[idMotor]%(360-48)
+        resto = posicao(idMotor)%(360-48)
         
-        while(matrix[idMotor] < 200):#estar entre 228 e 318
+        while(posicao(idMotor) < 200):#estar entre 228 e 318
             atualizar()
             print("2")
             irEsquerda(vel)
         idMotor = encoder()[1];#isso aqui tem que atualizar a parte de cima pra ser os motores que vão pra frente(ou não caso for igual)
         start = encoder()[0];
-        while(matrix[idMotor] < start + resto ):
+        while(posicao(idMotor) < start + resto ):
             atualizar()
             print("3")
             irEsquerda(vel)
@@ -104,7 +113,7 @@ def frente(vel):
     # tor(),motor3.setor(),motor4.setor()
     valorInicial = [motor1.setor()]
     print(valorInicial)
-    #and motor2.setor() == valorInicial[2] and motor3.setor() == valorInicial[3] and motor4.setor() == valorInicial[4]
+    #and motor2.setor() == valorInicial(2) and motor3.setor() == valorInicial[3] and motor4.setor() == valorInicial[4]
     while(motor1.setor() == valorInicial[0] or cont != 1 ):
         motor1.move(vel)
         motor2.move(-vel)
@@ -127,14 +136,14 @@ def frente(vel):
 
 def aux():
     atualizar()
-    if(matrix[3]< 138 or matrix[3] > 181): #usar outro motor pra pegar pos
-        return [matrix[3],3]
-    elif(matrix[2]< 138 or matrix[2] > 181):
-        return [matrix[2],2]
-    elif(matrix[1]< 138 or matrix[1] > 181):
-        return [matrix[1],1]
+    if(posicao(3)< 138 or posicao(3) > 181): #usar outro motor pra pegar pos
+        return [posicao(3),3]
+    elif(posicao(2)< 138 or posicao(2) > 181):
+        return [posicao(2),2]
+    elif(posicao(1)< 138 or posicao(1) > 181):
+        return [posicao(1),1]
     else:
-        return [matrix[0],0]
+        return [posicao(0),0]
 def frente2(vel):
     idMotor = aux()[1];#isso aqui tem que atualizar a parte de cima pra ser os motores que vão pra frente(ou não caso for igual)
     start = aux()[0];
@@ -145,38 +154,38 @@ def frente2(vel):
             a = start - 180
             if a < 0:
                 a += 360
-            while matrix[idMotor] > a: #-> se a for menor q 360 ele vai ser entre -48 e -180 então entra certo
+            while posicao(idMotor) > a: #-> se a for menor q 360 ele vai ser entre -48 e -180 então entra certo
                 atualizar()
                 irFrente(vel)
         else:
-            while matrix[idMotor] < (start + 180)%360:#se passar de 360 volta pro range normal
+            while posicao(idMotor) < (start + 180)%360:#se passar de 360 volta pro range normal
                 atualizar()
                 irFrente(vel)
     else:
         if lookup[idMotor]:
             resto = 180 - (360 - start)  
-            while(matrix[idMotor] <200):
+            while(posicao(idMotor) <200):
                 atualizar()
                 irFrente(vel)
             idMotor = aux()[1];#isso aqui tem que atualizar a parte de cima pra ser os motores que vão pra frente(ou não caso for igual)
             start = aux()[0];
             atualizar()
-            while matrix[idMotor] < start + resto:
+            while posicao(idMotor) < start + resto:
                 atualizar()
                 irFrente(vel)
         else:
             resto = 180 - start  
-            while(matrix[idMotor] >0):
+            while(posicao(idMotor) >0):
                 atualizar()
                 irFrente(vel)
             idMotor = aux()[1];#isso aqui tem que atualizar a parte de cima pra ser os motores que vão pra frente(ou não caso for igual)
             start = aux()[0];
             atualizar()
-            while(matrix[idMotor] > start - resto):
+            while(posicao(idMotor) > start - resto):
                 atualizar()
                 irFrente(vel)
         #ou menor n sei depende se aumenta ou diminui
-        # and motor2.setor() != valorInicial[2] and motor3.setor() != valorInicial[3] and motor4.setor() != valorInicial[4]
+        # and motor2.setor() != valorInicial(2) and motor3.setor() != valorInicial[3] and motor4.setor() != valorInicial[4]
 def irFrente(vel):
     motor1.move(vel)
     motor2.move(-vel)
