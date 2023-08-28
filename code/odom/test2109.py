a49a0e59ac439885b63536109cf306d170f77639
kinguim's pc <kinguim1000@gmail.com>
2023-08-28 18:15:46 -0300

    auto

--- code/odom/test2109.py
+++ code/odom/test2109.py
@@ -5,6 +5,40 @@ import string
 import funcAux.py
 LX16A.initialize("/dev/ttyUSB0")
 
+
+
+
+class PID:
+    def __init__(self,P,I,D):
+        self.kp = P
+        self.ki = I
+        self.kd = D
+        self.prev = 0
+        self.anterior = 0;
+    def atualizar(self,error):
+        self.prev += erro
+        self.anterior = erro
+    def out (self,erro):
+        self.atualizar(error)
+        return self.kp*erro + self.ki*self.prev + self.kd*(erro - self.anterior)
+
+def sign(a):
+    if a < 0:
+        return -1
+    if a == 0:
+        return 0
+    return 1
+def abs(a):
+    if a < 0:
+        return -a
+    return a
+def mapa(max,input):
+    return (max/(1+((max/2)/abs(input))))*sign(input)
+
+
+
+
+
 class motor:
     def __init__(self,porta):
         self.porta = porta
